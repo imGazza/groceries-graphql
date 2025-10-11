f8bcce6e6ffd88d4f52aa61ec1c0bef0ce468036
imGazza <gazzardi.luca@gmail.com>
2025-10-11 14:49:07 +0200

    updated authorized queries and bruno

--- Groceries-GraphQL/API/Schema/Mutations/Catalog/CatalogMutations.cs
+++ Groceries-GraphQL/API/Schema/Mutations/Catalog/CatalogMutations.cs
@@ -1,12 +1,14 @@
 ﻿
 using API.Records;
 using API.Services.Catalog;
+using HotChocolate.Authorization;
 
 namespace API.Schema.Mutations.Catalog
 {
     [ExtendObjectType(typeof(Mutation))]
     public class CatalogMutations
-    {           
+    {
+        [Authorize]
         public async Task<bool> CreateProduct(ProductInput productInput, IFile productImage, [Service] ICatalogService _catalogService)
         {
             if (productInput == null || productImage == null)
@@ -16,6 +18,7 @@ namespace API.Schema.Mutations.Catalog
             return true;
         }
 
+        [Authorize]
         public async Task<ProductItemOutput> UpdateProductImage(string productId, IFile productImage, [Service] ICatalogService _catalogService)
         {
             if (string.IsNullOrEmpty(productId) || productImage == null)
@@ -24,6 +27,7 @@ namespace API.Schema.Mutations.Catalog
             return await _catalogService.UpdateProductImage(productId, productImage);
         }
 
+        [Authorize]
         public async Task<bool> CreateCategory(CategoryInput categoryInput, [Service] ICatalogService _catalogService)
         {
             if (categoryInput == null)
